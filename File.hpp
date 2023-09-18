f2cac252d14b9ae275cb4b36e0813d7cf41515d7
opabinia <harold50261990@gmail.com>
2023-09-19 00:37:13 +0900

    q1done

--- File.hpp
+++ File.hpp
@@ -8,42 +8,42 @@
 #include <fstream>
 #include <sstream>
 
-using namespace std;
 
+//ファイルの読み込みのためのクラス
 class File{
     public:
-    vector<vector<string>> log;
+    std::vector<std::vector<std::string>> log;
 
     File(const char* filename){
-        ifstream ifs(filename, ios::in);
+        std::ifstream ifs(filename, std::ios::in);
         file_read(ifs);
     }
 
     File(){
-        ifstream ifs("example.csv", ios::in);
+        std::ifstream ifs("example.csv", std::ios::in);
         file_read(ifs);
     }
 
     void print(){
         for(auto log_line : log){
             for(auto log_element:log_line){
-                cout<<log_element<<" ";
+                std::cout<<log_element<<" ";
         }
-        cout<<"\n";
+        std::cout<<"\n";
     }
     }
 
     private:
-    void file_read(ifstream& ifs){
+    void file_read(std::ifstream& ifs){
         if(ifs.fail()){
-            cout<<"This File is not open."<<endl;
+            std::cout<<"This File is not open.\n";
             return;
         }
-        string line, element;
-        while(getline(ifs, line)){
+        std::string line, element;
+        while(std::getline(ifs, line)){
             std::istringstream line_stream(line);
-            vector<string> line_buf;
-            while(getline(line_stream, element,',')){
+            std::vector<std::string> line_buf;
+            while(std::getline(line_stream, element,',')){
                 line_buf.push_back(element);
             }
             log.push_back(line_buf);
